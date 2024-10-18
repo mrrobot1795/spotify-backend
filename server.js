fdf15153a5b8b62f08823b13e29aadf144092660
Shashidhar Sn <snshashidhar3@gmail.com>
2024-10-18 14:13:22 -0700

    updated serv

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
