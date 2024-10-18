0c11fa9e2c4c3a5ad62dda84ea4b0c8520bfcb39
Shashidhar Sn <snshashidhar3@gmail.com>
2024-10-18 13:49:17 -0700

    updated server

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ const corsOptions = {
     "http://localhost:3000"
   ],
   credentials: true,
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
+
+app.options('*', cors(corsOptions));
 
 app.use(cors(corsOptions));
 
